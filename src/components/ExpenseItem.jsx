c4bff87c7377565f1fbc0a289b13a098dd191798
Sean Schwabenlander <sean@schwabenlander.com>
2021-12-09 14:27:39 -0600

    Refactored the expense date into its own component

--- src/components/ExpenseItem.jsx
+++ src/components/ExpenseItem.jsx
@@ -1,16 +1,16 @@
+import ExpenseDate from './ExpenseDate';
 import './ExpenseItem.css';
-import DateItem from './DateItem';
 
 function ExpenseItem(props) {
-    return (
-        <div className="expense-item">
-            <DateItem date={props.date} />
-            <div className="expense-item__description">
-                <h2>{props.title}</h2>
-                <div className="expense-item__price">${props.amount}</div>
-            </div>
-        </div>
-    );
+  return (
+    <div className="expense-item">
+      <ExpenseDate date={props.date} />
+      <div className="expense-item__description">
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
+      </div>
+    </div>
+  );
 }
 
 export default ExpenseItem;

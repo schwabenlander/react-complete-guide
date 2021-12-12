e8333f7e058cbcc2f676ad1467d511d2ad07920c
Sean Schwabenlander <sean@schwabendesign.com>
2021-12-11 20:25:21 -0600

    Implemented component to filter expenses by year

--- src/components/Expense/ExpenseList.jsx
+++ src/components/Expense/ExpenseList.jsx
@@ -1,17 +1,38 @@
 import './ExpenseList.css';
-import './ExpenseItem';
+import './ExpenseItem.css';
 import ExpenseItem from './ExpenseItem';
 import Card from '../Common/Card';
+import ExpenseFilter from './ExpenseFilter';
+import { useState } from 'react';
 
 const ExpenseList = (props) => {
+  const [filterYear, setFilterYear] = useState('2020');
+
+  const handleFilterYearChanged = (newFilterYear) => {
+    setFilterYear(newFilterYear);
+    filterExpenses(newFilterYear);
+  };
+
+  const filterExpenses = (year) => {
+    console.log(
+      props.expenses.filter(
+        (expense) => expense.date.getFullYear().toString() === year
+      )
+    );
+  };
+
   return (
     <Card className="expense-list">
+      <ExpenseFilter
+        selectedYear={filterYear}
+        onFilterYearChanged={handleFilterYearChanged}
+      />
       {props.expenses.map((expense) => (
         <ExpenseItem
           title={expense.title}
           amount={expense.amount}
           date={expense.date}
-          key={expense.title}
+          key={expense.id}
         />
       ))}
     </Card>
